9acfb5931d7a27611974f3d928ac1768d2e66fc1
amenoni <alvaro.menoni@gmail.com>
2016-02-05 15:53:29 -0300

    closes #1

--- Termocontrol/Logs/admin.py
+++ Termocontrol/Logs/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import tempLog, usageLog
 # Register your models here.
 
-admin.site.register(tempLog)
+
+class tempLogAdmin(admin.ModelAdmin):
+    list_display = ('pk','timestamp_UTC','temp')
+    list_filter = ('timestamp_UTC',)
+
+class usageLogAdmin(admin.ModelAdmin):
+    list_display = ('pk','timestamp_UTC','weekday','hour','type',)
+    list_filter = ('timestamp_UTC','weekday','hour','type')
+
+
+admin.site.register(tempLog,tempLogAdmin)
 admin.site.register(usageLog)
